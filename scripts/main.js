b40b3374437451d5552d11ad04d56106df12ec98
Bruce rb Lee <51773505+leerenbing@users.noreply.github.com>
2020-04-30 16:52:47 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 let myHeading =document.querySelector("h1");
 myHeading.textContent ='Hello World';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/me.png') {
+      myImage.setAttribute('src', 'images/IMG_20200330_152306.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/IMG_20200323_200207.jpg');
+    }
+}
