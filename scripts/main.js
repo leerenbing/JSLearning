7cc35bb2373d6e24f1bbb4db208e4da50f849598
Bruce rb Lee <51773505+leerenbing@users.noreply.github.com>
2020-04-30 17:06:39 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,12 @@ myImage.onclick = function() {
       myImage.setAttribute('src', 'images/IMG_20200323_200207.jpg');
     }
 }
+
+const para = document.querySelector('.p1');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+  let name = prompt('输入一个新的名字：');
+  para.textContent = 'player1：' + name;
+}
